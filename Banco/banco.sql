2cfb56f395f1491b75de47dd9346ae61ceaa53ff
Ruan Felipe Staron <Ruan Felipe Staron>
2016-05-02 21:08:23 -0300

    Banco de dados

---
+++ Banco/banco.sql
@@ -0,0 +1,34 @@
+CREATE TABLE departamento (
+	id_departamento		SERIAL NOT NULL PRIMARY KEY,
+	descricao			VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE usuario (
+	id_usuario			SERIAL NOT NULL PRIMARY KEY,
+	nome				VARCHAR(255) NOT NULL UNIQUE,
+	senha				VARCHAR(50) NOT NULL,
+	id_departamento		INTEGER NOT NULL REFERENCES departamento(id_departamento),
+	email 				VARCHAR(255) UNIQUE CHECK (email ~* '^[A-Za-z0-9.%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+	tipo				BOOLEAN
+);
+
+CREATE TABLE motivo (
+	id_motivo			SERIAL NOT NULL PRIMARY KEY,
+	descricao			VARCHAR(255) NOT NULL UNIQUE,
+	--Se trata de um motivo quando a sala está indisponível
+	incidencia			VARCHAR (255)
+);
+
+CREATE TABLE sala (
+	id_sala				SERIAL NOT NULL PRIMARY KEY,
+	descricao			VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE reserva (
+	id_reserva			SERIAL NOT NULL PRIMARY KEY,
+	id_sala				INTEGER NOT NULL REFERENCES sala(id_sala),
+	id_usuario			INTEGER NOT NULL REFERENCES usuario(id_usuario),
+	data 				DATE NOT NULL,
+	hora				TIMESTAMP NOT NULL,
+	id_motivo			INTEGER NOT NULL REFERENCES motivo(id_motivo)
+);
