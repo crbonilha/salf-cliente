8ab268ff6aa5717955652239fd274971c086cf4f
Ruan Staron <ruan.staron@hotmail.com>
2016-05-09 13:52:56 -0300

    Pensei em criar uma classe que fosse responsável pela comunicação com o webservice, classe "envia" dentro dela o construtor seleciona a forma como vai enviar a mensagem, para essa seleção note que na view login, inclui um input do tipo hidden com o nome "formulario" e o valor "execLogin"

--- application/controllers/Envia.php
+++ application/controllers/Envia.php
@@ -3,10 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Envia extends CI_Controller {
 
+	function logar($login, $senha){
+		$mensagem = json_encode(array($login,$senha));
+		$resultado = file_get_contents('endereço_do_servidor/classe/metodo?login='.$login.'&senha='.$senha);
+	}
+
 	public function index()
 	{
-		$login = array ($_POST['login'], $_POST['senha']);
-		$json = json_encode($login);
-		echo $json;
+		switch ($_POST['formulario']) {
+			case 'execLogin':
+				$this->logar($_POST['login'], $_POST['senha']);
+				break;
+			
+			default:
+				$data['titulo'] = null;
+				$this -> load -> view('header', $data);
+				$this -> load -> view('login', $data);
+				$this -> load -> view('footer', $data);
+				break;
+		}
 	}
 }
--- application/views/Login.php
+++ application/views/Login.php
@@ -6,13 +6,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div id="container">
 			<h1>Login</h1>
 				<form method="POST" action="<?php echo base_url();?>index.php/envia">
+					<input type="hidden" name="formulario" value="execLogin">
 					<table cellspacing="10">
 						<tr>
 							<td>
 							<label>Login: </label>
 							</td>
 							<td align="left">
-								<input type="text" name="login" id="login">
+								<input type="text" name="login" id="login" placeholder="Digite seu login aqui">
 							</td>
 						</tr>
 						<tr>
@@ -20,15 +21,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								<label>Senha: </label>
 							</td>
 							<td align="left">
-								<input type="password" name="senha" id="senha">
+								<input type="password" name="senha" id="senha" placeholder="Digite sua senha aqui">
 							</td>
 						</tr>
 						<tr>
 							<td>
-								<input type="submit" id="enviar" name="enviar">
+								<input type="submit" id="enviar">
 							</td>
 							<td>
-								<input type="reset" value="Limpar">
+								<input type="reset" id="limpar" value="Limpar">
 							</td>
 						</tr>
 					</table>
