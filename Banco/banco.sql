7f0db1bc637bdf0df37858f3991bfa46ef4e8549
Ruan Staron <ruan.staron@hotmail.com>
2016-05-13 08:30:13 -0300

    Alterado banco de dados para já inserir alguns dados no momento que é executado, sugiro dropar e cadastrar novamente o banco

--- Banco/banco.sql
+++ Banco/banco.sql
@@ -29,6 +29,21 @@ CREATE TABLE reserva (
 	id_sala				INTEGER NOT NULL REFERENCES sala(id_sala),
 	id_usuario			INTEGER NOT NULL REFERENCES usuario(id_usuario),
 	data 				DATE NOT NULL,
-	hora				TIMESTAMP NOT NULL,
+	hora				TIME NOT NULL,
 	id_motivo			INTEGER NOT NULL REFERENCES motivo(id_motivo)
 );
+
+--Inserção de dados
+INSERT INTO public.departamento(descricao) VALUES ('Informática');
+INSERT INTO public.departamento(descricao) VALUES ('Engenharia elétrica');
+INSERT INTO public.departamento(descricao) VALUES ('Engenharia mecânica');
+INSERT INTO public.usuario(nome, senha, id_departamento, email, tipo) VALUES ('admin', 'admin', 1, 'administracao@email.com', true);
+INSERT INTO public.usuario(nome, senha, id_departamento, email, tipo) VALUES ('user', 'user', 1, 'usuario@email.com', false);
+INSERT INTO public.motivo(descricao) VALUES ('Aula');
+INSERT INTO public.motivo(descricao, incidencia) VALUES ('Indisponível', 'Computador queimado');
+INSERT INTO public.motivo(descricao) VALUES ('Prova');
+INSERT INTO public.sala(descricao) VALUES ('Lab1');
+INSERT INTO public.sala(descricao) VALUES ('Lab2');
+INSERT INTO public.sala(descricao) VALUES ('Lab3');
+INSERT INTO public.reserva(id_sala, id_usuario, data, hora, id_motivo) VALUES (1, 1, '2016-05-12', '17:00', 1);
+INSERT INTO public.reserva(id_sala, id_usuario, data, hora, id_motivo) VALUES (2, 2, '2016-05-13', '19:00', 2);
