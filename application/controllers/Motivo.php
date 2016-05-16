9d799b5293e5cfd0da1e793b6ab8967b38777fce
Cristhian Bonilha <cristhian.bonilha@hotmail.com>
2016-05-16 20:34:00 -0300

    - adicionados campos na interface para se realizar cadastro e alteração de motivos

--- application/controllers/Motivo.php
+++ application/controllers/Motivo.php
@@ -35,13 +35,51 @@ class Motivo extends CI_Controller {
 		curl_exec($ch);
 	}
 
+	public function altera_motivo($id_motivo, $descricao) {
+		$data = array('id_motivo' => $id_motivo, 'descricao' => $descricao);
+		$data_string = json_encode($data);
+
+		$ch = curl_init('http://localhost:8080/salf-server/webresources/salf_server/alteraMotivo');
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type: application/json',
+		    'Content-Length: ' . strlen($data_string)
+		));
+
+		curl_exec($ch);
+	}
+
+	public function cadastra_motivo($descricao) {
+		$data = array('descricao' => $descricao);
+		$data_string = json_encode($data);
+
+		$ch = curl_init('http://localhost:8080/salf-server/webresources/salf_server/cadastraMotivo');
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type: application/json',
+		    'Content-Length: ' . strlen($data_string)
+		));
+
+		curl_exec($ch);
+	}
+
 	public function index() {
 		$data['titulo'] = 'Motivos de reserva';
 		$data['estilos'] = array('motivo');
 		$data['admin'] = true;
 
 		if($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$this -> exclui_motivo($_POST['id_motivo']);
+			if(isset($_POST['excluir']) && isset($_POST['id_motivo'])) {
+				$this -> exclui_motivo($_POST['id_motivo']);
+			} else if(isset($_POST['alterar']) && isset($_POST['id_motivo']) && isset($_POST['descricao'])) {
+				$this -> altera_motivo($_POST['id_motivo'], $_POST['descricao']);
+			} else if(isset($_POST['cadastrar']) && isset($_POST['descricao'])) {
+				$this -> cadastra_motivo($_POST['descricao']);
+			}
 		}
 
 		$data['motivos'] = $this -> lista_motivos();
--- application/views/motivo.php
+++ application/views/motivo.php
@@ -28,6 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<?php if($admin === true) { ?>
 							<td>
 								<form method="post" action="<?php echo base_url(); ?>index.php/motivo" class="inline">
+									<input type="hidden" name="excluir"/>
 									<button type="submit" name="id_motivo" value="<?php echo $motivo['id_motivo']; ?>" class="link-button">Excluir</button>
 								</form>
 							</td>
@@ -35,8 +36,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</tr>
 				<?php } ?>
 			</table>
+			<h1>Alterar motivo</h1>
+			<form method="post" action="<?php echo base_url(); ?>index.php/motivo">
+				<input type="hidden" name="alterar" value="alterar" />
+				<table cellspacing="10">
+					<tr>
+						<td>
+							<label>Id</label>
+						</td>
+						<td>
+							<input type="number" name="id_motivo" placeholder="Digite o id" required></input>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>Descrição</label>
+						</td>
+						<td>
+							<input type="text" name="descricao" placeholder="Digite a descrição" required></input>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<input type="submit" id="cadastrar"></input>
+						</td>
+						<td>
+							<input type="reset" id="limpar"></input>
+						</td>
+					</tr>
+				</table>
+			</form>
 			<h1>Cadastrar novo motivo</h1>
-			<form method="POST" action="#">
+			<form method="post" action="<?php echo base_url(); ?>index.php/motivo">
+				<input type="hidden" name="cadastrar" value="cadastrar" />
 				<table cellspacing="10">
 					<tr>
 						<td>
