92662510ee27242fd701f30824ffa9d3406f8e26
Ruan Staron <ruan.staron@hotmail.com>
2016-06-22 17:05:47 -0300

    - Ajustes na reserva

--- application/controllers/Reserva.php
+++ application/controllers/Reserva.php
@@ -17,6 +17,14 @@ class Reserva extends CI_Controller {
         $data['get_http'] = $this -> reserva_modelo -> lista(null);
 		$data['reservas'] = json_decode($data['get_http']['response_body_ne'], true);
 
+		$this -> load -> model('sala_modelo');
+		$data['get_http'] = $this -> sala_modelo -> lista(null);
+		$data['salas'] = json_decode($data['get_http']['response_body_ne'], true);
+
+		$this -> load -> model('motivo_modelo');
+		$data['get_http'] = $this -> motivo_modelo -> lista(null);
+		$data['motivos'] = json_decode($data['get_http']['response_body_ne'], true);
+
 		$this -> load -> view('header', $data);
 		$this -> load -> view('reserva', $data);
 		$this -> load -> view('footer', $data);
--- application/views/reserva.php
+++ application/views/reserva.php
@@ -4,122 +4,83 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="content">
 	<section>
 		<div id="container">
-			<div id="listar_reserva">
-				<h1>Lista de reservas</h1>
-				<table cellspacing="10">
-					<tr>
-						<td>
-							<label>Id</label>
-						</td>
-						<td>
-							<label>Sala</label>
-						</td>
-						<td>
-							<label>Motivo</label>
-						</td>
-						<td>
-							<label>Data</label>
-						</td>
-						<td>
-							<label>Hora</label>
-						</td>
-						<?php if($admin === true) { ?>
-							<td></td>
-						<?php } ?>
-					</tr>
-					<?php if(isset($reservas)) foreach($reservas as $reserva) { ?>
+			<section class="box right">
+				<div id="listar_reserva">
+					<h1>Lista de reservas</h1>
+					<table cellspacing="10">
 						<tr>
 							<td>
-								<?php echo $reserva['id']; ?>
+								<label>Id</label>
 							</td>
 							<td>
-								<?php echo $reserva['sala']; ?>
+								<label>Sala</label>
 							</td>
 							<td>
-								<?php echo $reserva['motivo']; ?>
+								<label>Motivo</label>
 							</td>
 							<td>
-								<?php echo $reserva['data']; ?>
+								<label>Data</label>
 							</td>
 							<td>
-								<?php echo $reserva['id_horario']; ?>
+								<label>Hora</label>
 							</td>
 							<?php if($admin === true) { ?>
-								<td>
-									<form class="inline" method="post" action="<?php echo base_url(); ?>index.php/reserva">
-										<input type="hidden" name="formulario" value="excluir" />
-										<button type="submit" name="id" value="<?php echo $reserva['id']; ?>" class="link-button">Excluir</button>
-									</form>
-								</td>
+								<td></td>
 							<?php } ?>
 						</tr>
-					<?php } ?>
-				</table>
-			</div>
-			<h1>Alterar reserva</h1>
-			<form method="post" action="<?php echo base_url(); ?>index.php/reserva">
-				<input type="hidden" name="formulario" value="alterar" />
-				<table cellspacing="10">
-					<tr>
-						<td>
-							<label>Id</label>
-						</td>
-						<td>
-							<input type="number" min="1" name="id" placeholder="Digite o id" required></input>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>Sala</label>
-						</td>
-						<td>
-							<input type="number" name="sala" required></input>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>Motivo</label>
-						</td>
-						<td>
-							<input type="number" name="motivo" required></input>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>Data</label>
-						</td>
-						<td>
-							<input type="text" name="data" required></input>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>Hora</label>
-						</td>
-						<td>
-							<input type="number" name="id_horario" required></input>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<input type="submit" id="cadastrar"></input>
-						</td>
-						<td>
-							<input type="reset" id="limpar"></input>
-						</td>
-					</tr>
-				</table>
-			</form>
-			<h1>Cadastrar nova reserva</h1>
+						<?php if(isset($reservas)) foreach($reservas as $reserva) { ?>
+							<tr>
+								<td>
+									<?php echo $reserva['id']; ?>
+								</td>
+								<td>
+									<?php echo $reserva['sala']; ?>
+								</td>
+								<td>
+									<?php echo $reserva['motivo']; ?>
+								</td>
+								<td>
+									<?php echo $reserva['data']; ?>
+								</td>
+								<td>
+									<?php echo $reserva['id_horario']; ?>
+								</td>
+								<?php if($admin === true) { ?>
+									<td>
+										<form class="inline" method="post" action="<?php echo base_url(); ?>index.php/reserva">
+											<input type="hidden" name="formulario" value="excluir" />
+											<button type="submit" name="id" value="<?php echo $reserva['id']; ?>" class="link-button">Excluir</button>
+										</form>
+									</td>
+								<?php } ?>
+							</tr>
+						<?php } ?>
+					</table>
+				</div>
+			</section>
+			<section class="box left">
+				<h1>Alterar reserva</h1>
 				<form method="post" action="<?php echo base_url(); ?>index.php/reserva">
-					<input type="hidden" name="formulario" value="cadastrar" />
+					<input type="hidden" name="formulario" value="alterar" />
 					<table cellspacing="10">
+						<tr>
+							<td>
+								<label>Id</label>
+							</td>
+							<td>
+								<input type="number" min="1" name="id" placeholder="Digite o id" required></input>
+							</td>
+						</tr>
 						<tr>
 							<td>
 								<label>Sala</label>
 							</td>
 							<td>
-								<input type="number" name="sala" required></input>
+								<select name="sala">
+									<?php if(isset($salas)) foreach($salas as $sala) { ?>
+										<option value="<?php echo $sala['id']; ?>"><?php echo $sala['descricao']; ?></option>
+									<?php } ?>
+								</select>
 							</td>
 						</tr>
 						<tr>
@@ -127,7 +88,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								<label>Motivo</label>
 							</td>
 							<td>
-								<input type="number" name="motivo" required></input>
+								<select name="motivo">
+									<?php if(isset($motivos)) foreach($motivos as $motivo) { ?>
+										<option value="<?php echo $motivo['id']; ?>"><?php echo $motivo['descricao']; ?></option>
+									<?php } ?>
+								</select>
 							</td>
 						</tr>
 						<tr>
@@ -135,7 +100,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								<label>Data</label>
 							</td>
 							<td>
-								<input type="text" name="data" required></input>
+								<input type="date" name="data" required></input>
 							</td>
 						</tr>
 						<tr>
@@ -143,7 +108,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								<label>Hora</label>
 							</td>
 							<td>
-								<input type="number" name="id_horario" required></input>
+								<input type="number" min="1" name="id_horario" required></input>
 							</td>
 						</tr>
 						<tr>
@@ -156,6 +121,61 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						</tr>
 					</table>
 				</form>
+				<h1>Cadastrar nova reserva</h1>
+					<form method="post" action="<?php echo base_url(); ?>index.php/reserva">
+						<input type="hidden" name="formulario" value="cadastrar" />
+						<table cellspacing="10">
+							<tr>
+								<td>
+									<label>Sala</label>
+								</td>
+								<td>
+									<select name="sala">
+										<?php if(isset($salas)) foreach($salas as $sala) { ?>
+											<option value="<?php echo $sala['id']; ?>"><?php echo $sala['descricao']; ?></option>
+										<?php } ?>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<label>Motivo</label>
+								</td>
+								<td>
+									<select name="motivo">
+										<?php if(isset($motivos)) foreach($motivos as $motivo) { ?>
+											<option value="<?php echo $motivo['id']; ?>"><?php echo $motivo['descricao']; ?></option>
+										<?php } ?>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<label>Data</label>
+								</td>
+								<td>
+									<input type="date" name="data" required></input>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<label>Hora</label>
+								</td>
+								<td>
+									<input type="number" min="1" name="id_horario" required></input>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="submit" id="cadastrar"></input>
+								</td>
+								<td>
+									<input type="reset" id="limpar"></input>
+								</td>
+							</tr>
+						</table>
+					</form>
+			</section>
 		</div>
 	</section>
 </div>
